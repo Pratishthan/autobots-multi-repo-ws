2acf8295eb112d16266ae9f7db044e1d601eacd6
PK <pro.kamath@gmail.com>
2026-02-14 20:56:01 +0530

    Refactor sanity.sh to include cleanup and cloning of shared libraries

--- sanity.sh
+++ sanity.sh
@@ -1,7 +1,24 @@
+# Grab the .env file
+cp autobots-agents-jarvis/.env .env.sanity
+
+# Cleanup
+rm -rf autobots-devtools-shared-lib
+rm -rf autobots-agents-jarvis
+
+# Clone the repositories
+git clone https://github.com/Pratishthan/autobots-devtools-shared-lib.git
+
+git clone https://github.com/Pratishthan/autobots-agents-jarvis.git
+cd autobots-agents-jarvis
+git checkout develop
+cd ..
+
+# Setup
 make clean
 make setup
 source .venv/bin/activate
 make install
 make install-dev
+cp .env.sanity autobots-agents-jarvis/.env
 cd autobots-agents-jarvis
 make sanity
